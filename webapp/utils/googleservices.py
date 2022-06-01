64e83dba2157b3e1d177c8f00103237d2bcc026a
Shub Raj Lama <shuvraj1234@gmail.com>
2022-06-01 22:47:29 +0545

    modified

--- webapp/utils/googleservices.py
+++ webapp/utils/googleservices.py
@@ -1,7 +1,13 @@
-from  ..instaPrivate.instagram import insta
+from ..instaPrivate.instagram import insta
+import requests
+
+
 def verifyCaptcha(response):
     try:
-        resp = insta.session.get(f"https://www.google.com/recaptcha/api/siteverify?secret=6LeKlDAgAAAAAPUAVbCR7w5_XQrmQJDWVGU3Rhyj&response={response}").json()
+        resp = insta.session.get(
+            f"https://www.google.com/recaptcha/api/siteverify?secret=6LeKlDAgAAAAAPUAVbCR7w5_XQrmQJDWVGU3Rhyj&response={response}")
+        if resp.status_code != 200:
+            return False
     except Exception as e:
         return False
-    return resp.get("success")
+    return resp.json().get("success")
